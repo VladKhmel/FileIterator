4b186c5083d3cb7df6b24fac3a9b29dc0baa31b0
VladKhmel <vladkhmel22@gmail.com>
2022-12-31 17:10:11 +0300

    fixed iterator

--- src/FileIterator.java
+++ src/FileIterator.java
@@ -2,45 +2,50 @@ import java.io.*;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-class FileIterator implements Iterator<String> {
+class FileIterator implements Iterator<String>,AutoCloseable {
 
     private String path;
-    private BufferedReader reader;
+    private final BufferedReader reader;
+
+    private IOException lastException;
+
 
     public FileIterator(String path) throws FileNotFoundException {
         this.path = path;
         try {
             reader = new BufferedReader(new FileReader(path));
         } catch (FileNotFoundException e) {
-            throw new FileNotFoundException(e.getMessage());
+            throw new FileNotFoundException();
         }
     }
-
+    public IOException ioException() {
+        return lastException;
+    }
+    @Override
+    public void close() throws IOException {
+        reader.close();
+    }
     @Override
     public boolean hasNext() {
         boolean hasNext = false;
         try {
             hasNext = reader.ready();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            lastException = e;
         }
         return hasNext;
     }
 
     @Override
     public String next() throws NoSuchElementException {
-        if (!hasNext()) {
-            try {
-                reader.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            throw new NoSuchElementException("NoSuchElementException");
-        }
+
         try {
-            return reader.readLine();
+            if(hasNext()){
+                return reader.readLine();
+            }
+            throw new NoSuchElementException();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new UncheckedIOException(e);
         }
     }
-};
+}
